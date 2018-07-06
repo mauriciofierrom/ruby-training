17209e8afb37325b2c19398eeecf34f2fc0474cb
Mauricio Fierro <mauriciofierrom@gmail.com>
2018-07-06 11:06:29 -0500

    Arel join

--- t1/rails/pokemon-arel/app/main.rb
+++ t1/rails/pokemon-arel/app/main.rb
@@ -3,9 +3,12 @@ require 'pp'
 Dir['./app/models/*'].each {|f| load f}
 
 golduck = Pokemon.where(number: 55)
-
 water_type = Type.where(name: 'Water')
 
-puts "All moves of type water from generation 7"
-# Pokemon.joins({ moves: :move_origins }).where('moves.type_id=1 AND move_origins.generation=7')
-# golduck.moves.joins(Move.join_by_origin_by_type(water_type))
+# All Pokemons with moves of type water that appear on generation 7
+pp golduck.moves.joins(Move.join_by_origin_by_type(water_type))
+pp Pokemon
+  .joins({ moves: :move_origins })
+  .where(["moves.type_id=? AND move_origins.generation=?",
+          golduck.id,
+          water_type.id])
--- t1/rails/pokemon-arel/app/models/pokemon.rb
+++ t1/rails/pokemon-arel/app/models/pokemon.rb
@@ -3,20 +3,32 @@ class Pokemon < ActiveRecord::Base
   has_many :move_sets
   has_many :moves, through: :move_sets
 
-  def find_by_move_type_and_generation(generation, type)
-    pkmn = self.arel_table
+  def find_by_move_type_and_generation(generation, the_type)
+    pokemon = self.class.arel_table
 
     type = Type.arel_table
     move_origin = MoveOrigin.arel_table
     move = Move.arel_table
     move_set = MoveSet.arel_table
 
-    move_set_join = a.create_on(
-      move_set[:pokemon_id].eq(pkmn[:id])
-      .and(move_set[:move_id].eq(move[:id]))
-      .and(move_origin[:move_id].eq(move[:id]))
-      .and(move_origin[:generation].eq(generation))
-      .and(type[:id].eq(type.id))
+    move_set_on = pokemon.create_on(
+      move_set[:pokemon_id].eq(pokemon[:id]))
+    move_set_join = pokemon.create_join(move_set, move_set_on)
+
+    move_on = move_set_join.create_on(
+      move_set[:move_id].eq(move[:id]))
+    move_join = move_set_join.create_join(move, move_on)
+
+    move_origin_on = move_join.create_on(
+      move_origin[:move_id].eq(move[:id])
+      .and(move_origin[:generation].eq(generation)))
+    move_origin_join = move_join.create_join(move_origin, move_origin_on)
+
+    type_on = move_origin_join.create_on(
+      type[:id].eq(the_type.id)
       .and(type[:id].eq(move[:type_id])))
+    type_join = move_origin_join.create_join(type, type_on)
+
+    Pokemon.joins(move_set_join, move_join, move_origin_join, type_join).where(id: id)
   end
 end
