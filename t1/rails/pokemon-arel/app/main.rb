069590ce26572cbe268c143d3068737bef709ce2
Mauricio Fierro <mauriciofierrom@gmail.com>
2018-07-06 17:27:40 -0500

    WIP Rails engines WIP

--- t1/rails/pokemon-arel/app/main.rb
+++ t1/rails/pokemon-arel/app/main.rb
@@ -6,9 +6,12 @@ golduck = Pokemon.where(number: 55)
 water_type = Type.where(name: 'Water')
 
 # All Pokemons with moves of type water that appear on generation 7
+# While generating the graph of the Arel-based query I ran out of memory D:
 pp golduck.moves.joins(Move.join_by_origin_by_type(water_type))
 pp Pokemon
   .joins({ moves: :move_origins })
   .where(["moves.type_id=? AND move_origins.generation=?",
           golduck.id,
           water_type.id])
+
+Pokemon.joins({ moves: :move_origins }).where(["moves.type_id=? AND move_origins.generation=?", golduck.id, water_type.id])
--- t1/rails/pokemon-arel/app/models/pokemon.rb
+++ t1/rails/pokemon-arel/app/models/pokemon.rb
@@ -6,7 +6,7 @@ class Pokemon < ActiveRecord::Base
   def find_by_move_type_and_generation(generation, the_type)
     pokemon = self.class.arel_table
 
-    type = Type.arel_table
+    # type = Type.arel_table
     move_origin = MoveOrigin.arel_table
     move = Move.arel_table
     move_set = MoveSet.arel_table
@@ -16,7 +16,8 @@ class Pokemon < ActiveRecord::Base
     move_set_join = pokemon.create_join(move_set, move_set_on)
 
     move_on = move_set_join.create_on(
-      move_set[:move_id].eq(move[:id]))
+      move_set[:move_id].eq(move[:id])
+      .and(move[:type_id].eq(the_type.id)))
     move_join = move_set_join.create_join(move, move_on)
 
     move_origin_on = move_join.create_on(
@@ -24,11 +25,12 @@ class Pokemon < ActiveRecord::Base
       .and(move_origin[:generation].eq(generation)))
     move_origin_join = move_join.create_join(move_origin, move_origin_on)
 
-    type_on = move_origin_join.create_on(
-      type[:id].eq(the_type.id)
-      .and(type[:id].eq(move[:type_id])))
-    type_join = move_origin_join.create_join(type, type_on)
+    # type_on = move_origin_join.create_on(
+    #   type[:id].eq(the_type.id)
+    #   .and(type[:id].eq(move[:type_id])))
+    # type_join = move_origin_join.create_join(type, type_on)
 
-    Pokemon.joins(move_set_join, move_join, move_origin_join, type_join).where(id: id)
+    # Pokemon.joins(move_set_join, move_join, move_origin_join, type_join).where(id: id)
+    Pokemon.joins(move_set_join, move_join, move_origin_join).where(id: id)
   end
 end
